1d62f4b3732df452e2991383073bc7fa456d1665
amingilani <amin@gilani.me>
2016-11-02 11:11:34 +0500

    Rubocop autofix

--- app/controllers/btc_addresses_controller.rb
+++ app/controllers/btc_addresses_controller.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 class BtcAddressesController < ApplicationController
   # before_action :authenticate_user!
   # def create
--- spec/controllers/btc_addresses_controller_spec.rb
+++ spec/controllers/btc_addresses_controller_spec.rb
@@ -1,5 +1,5 @@
+# frozen_string_literal: true
 require 'rails_helper'
 
 RSpec.describe BtcAddressesController, type: :controller do
-
 end
