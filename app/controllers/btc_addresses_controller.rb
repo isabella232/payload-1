fc75612500b2a7d60e28b2d018c7fe69779c788a
amingilani <amin@gilani.me>
2016-11-10 15:23:19 +0500

    Add a button to regenerate a new bitcoin address

--- app/controllers/btc_addresses_controller.rb
+++ app/controllers/btc_addresses_controller.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 class BtcAddressesController < ApplicationController
-  # before_action :authenticate_user!
-  # def create
-  #   #code
-  # end
+  before_action :authenticate_user!
+
+  def create
+    current_user.btc_addresses << BtcAddress.new
+    flash[:notice] = 'We created a new address for you!'
+    redirect_to dashboard_path
+  end
 end
