d16455e3ab8c71729238d815003129d6d3fa79e9
amingilani <amin@gilani.me>
2016-11-16 20:47:00 +0500

    Run the confirmation check on every block

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 class NotificationsController < ApplicationController
   # generally dangerious but this controller holds webhooks
-  skip_before_action :verify_authenticity_token, only: [:coinbase]
+  skip_before_action :verify_authenticity_token, only: [:coinbase, :mover]
 
   def coinbase
     # If the secret is incorrect, drop it like it's hot
@@ -16,5 +16,13 @@ class NotificationsController < ApplicationController
     btc_address = BtcAddress.find_by(address: address)
     btc_address.btc_txes << BtcTx.new(amount: amount, coinbase_id: coinbase_id)
     render json: { success: true, message: 'Transaction noted' }
+  end
+
+  def mover
+    unless params[:secret] == Rails.application.secrets.mover_webhook_secret
+      return render json: { success: false, message: 'Invalid secret' }
+    end
+    # If the secret's good, just keep going
+    UpdateBtcTxStatusJob.perform_now
   end
 end
